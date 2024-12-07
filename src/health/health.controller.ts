2c73c7fdeb1d7fa02cc514ae6240949e2bf18039
Sajan Emmanuel <sajanemmanuel@hotmail.com>
2024-12-07 16:33:26 +0530

    Fixed health check

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { HealthCheck, HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
+    private logger = new Logger("HealthCheck");
     constructor(
         private health: HealthCheckService,
-        private http: HttpHealthIndicator,
+        private http: HttpHealthIndicator
     ) { }
 
     @Get()
     @HealthCheck()
     check() {
+        // this.logger.log('Entering health check');
         return this.health.check([
             () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
         ]);
